2d32e7407d92621478e54cd6f3f487c62551b629
mcgabrielokurishi <mcgabrielokurishi@gmail.com>
2026-04-27 08:09:15 +0100

    Update vendor.module.ts

--- src/vendor/vendor.module.ts
+++ src/vendor/vendor.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { HttpModule } from '@nestjs/axios';
 import { VendingController } from './vendor.controller';
 import { VendingService } from './vendor.service';
+import { ConfigModule } from '@nestjs/config';
 import { WalletModule } from 'src/wallet/wallet.module';
 
 @Module({
   imports: [
     HttpModule,
     WalletModule,
+    ConfigModule
   ],
   controllers: [VendingController],
   providers: [VendingService],
