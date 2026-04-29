cebd55fbc961c4366baa5c6397f8ac9830427670
McGabriel Okurishi <mcgabrielokurishi@gmail.com>
2026-04-29 14:59:04 +0100

    add notification to vend

--- src/vendor/vendor.module.ts
+++ src/vendor/vendor.module.ts
@@ -3,6 +3,7 @@ import { HttpModule } from '@nestjs/axios';
 import { VendingController } from './vendor.controller';
 import { ConfigModule } from '@nestjs/config';
 import { VendingService } from './vendor.service';
+import { NotificationModule } from 'src/notification/notification.module';
 import { WalletModule } from 'src/wallet/wallet.module';
 
 @Module({
@@ -10,7 +11,8 @@ import { WalletModule } from 'src/wallet/wallet.module';
     HttpModule,
     ConfigModule,
     WalletModule,
-    ConfigModule
+    ConfigModule,
+    NotificationModule
   ],
   controllers: [VendingController],
   providers: [VendingService],
