ea33cb89cd6dad355ce016e1141c8b448780cf40
McGabriel Okurishi <mcgabrielokurishi@gmail.com>
2026-05-06 13:54:40 +0100

    resolve forcast

--- src/forcast/forcast.module.ts
+++ src/forcast/forcast.module.ts
@@ -3,11 +3,14 @@ import { ForecastController } from './forcast.controller';
 import { ForecastService } from './forcast.service';
 import { PushNotificationModule } from 'src/push-notification/push-notification.module';
 import { NotificationModule } from 'src/notification/notification.module';
+import { VendingModule } from 'src/vendor/vendor.module';
 
 @Module({
   imports: [
     PushNotificationModule,
     NotificationModule,
+    VendingModule,
+
   ],
   controllers: [ForecastController],
   providers:   [ForecastService],
