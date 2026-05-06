0c3630f8e0cb9736dabd1cb84f045547ef0c6652
McGabriel Okurishi <mcgabrielokurishi@gmail.com>
2026-05-06 14:10:53 +0100

    feat: integrate PushNotificationService into NotificationModule and update WalletModule imports

--- src/notification/notification.module.ts
+++ src/notification/notification.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { NotificationController } from './notification.controller';
 import { NotificationService } from './notification.service';
+import { PushNotificationService } from 'src/push-notification/push-notification.service';
 
 @Module({
   controllers: [NotificationController],
   providers:   [NotificationService],
-  exports:     [NotificationService], //  exported so other services can use it
+  exports:     [NotificationService,PushNotificationService] //  exported so other services can use it
 })
 export class NotificationModule {}
--- src/wallet/wallet.module.ts
+++ src/wallet/wallet.module.ts
@@ -3,12 +3,13 @@ import { WalletService } from "./wallet.service";
 import { WalletController } from "./wallet.controller";
 import { PrismaModule } from "database/database.module";
 import { AuthModule } from "../auth/auth.module";
+import { NotificationModule } from "src/notification/notification.module";
 import { BuypowerModule } from "../buypower/buypower.module";
 import { forwardRef } from "@nestjs/common";
 import { PushNotificationModule } from "src/push-notification/push-notification.module";
 
 @Module({
-  imports: [PrismaModule, AuthModule,PushNotificationModule, BuypowerModule,forwardRef(() => AuthModule)],
+  imports: [PrismaModule,NotificationModule, AuthModule,PushNotificationModule, BuypowerModule,forwardRef(() => AuthModule)],
   controllers: [WalletController],
   providers: [WalletService],
   exports: [WalletService],
