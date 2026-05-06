3a3acee03a22af3930ef5dc5198512b268ad32e7
McGabriel Okurishi <mcgabrielokurishi@gmail.com>
2026-05-06 15:20:09 +0100

    feat: add PushNotificationModule to VendingModule imports

--- src/vendor/vendor.module.ts
+++ src/vendor/vendor.module.ts
@@ -5,12 +5,14 @@ import { ConfigModule } from '@nestjs/config';
 import { VendingService } from './vendor.service';
 import { NotificationModule } from 'src/notification/notification.module';
 import { WalletModule } from 'src/wallet/wallet.module';
+import { PushNotificationModule } from 'src/push-notification/push-notification.module';
 
 @Module({
   imports: [
     HttpModule,
     ConfigModule,
     WalletModule,
+    PushNotificationModule,
     ConfigModule,
     NotificationModule
   ],
