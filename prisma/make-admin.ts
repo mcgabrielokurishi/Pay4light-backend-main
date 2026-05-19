b17c74a353df3aafb7eccee01b75e07974acd6da
McGabriel Okurishi <mcgabrielokurishi@gmail.com>
2026-05-19 12:48:00 +0100

    debug1

--- prisma/make-admin.ts
+++ prisma/make-admin.ts
@@ -1,13 +1,12 @@
-// prisma/make-admin.ts
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
+// import { PrismaClient } from '@prisma/client';
+// const prisma = new PrismaClient();
 
-async function main() {
-  await prisma.user.update({
-    where: { email: 'your-email@gmail.com' },
-    data:  { role: 'ADMIN' },
-  });
-  console.log(' Admin role granted');
-}
+// async function main() {
+//   await prisma.user.update({
+//     where: { email: 'your-email@gmail.com' },
+//     data:  { role: 'ADMIN' },
+//   });
+//   console.log(' Admin role granted');
+// }
 
-main().finally(() => prisma.$disconnect());
+// main().finally(() => prisma.$disconnect());
