8d35362af50933026dde324bd0c042e3af2d14ff
McGabriel Okurishi <mcgabrielokurishi@gmail.com>
2026-05-21 13:36:03 +0100

    debug7

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,27 +1,38 @@
-import { Module } from "@nestjs/common";
+import { Module, forwardRef } from "@nestjs/common";
 import { PassportModule } from "@nestjs/passport";
 import { JwtModule } from "@nestjs/jwt";
+
 import { AuthController } from "./auth.controller";
 import { JwtStrategy } from "./jwt.strategy";
 import { AuthService } from "./auth.service";
+
 import { PushNotificationModule } from "src/push-notification/push-notification.module";
 import { OtpModule } from "./OTP/otp.module";
 import { WalletModule } from "src/wallet/wallet.module";
-import { forwardRef } from "@nestjs/common";
+
+import { NotificationSettingsModule } from "src/notification-settings/notification-settings.module";
 
 @Module({
   imports: [
     PassportModule,
+
     JwtModule.register({
       secret: process.env.JWT_SECRET,
       signOptions: { expiresIn: "1h" },
     }),
+
     OtpModule,
     PushNotificationModule,
-    forwardRef(() => WalletModule)
+
+    forwardRef(() => WalletModule),
+
+    NotificationSettingsModule, // ADD THIS
   ],
+
   controllers: [AuthController],
+
   providers: [AuthService, JwtStrategy],
+
   exports: [JwtModule, AuthService],
 })
 export class AuthModule {}
