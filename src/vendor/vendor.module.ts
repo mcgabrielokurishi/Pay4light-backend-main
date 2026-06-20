5748c82a3776045d3cacff8944bc96af6687dae6
McGabriel Okurishi <mcgabrielokurishi@gmail.com>
2026-06-20 05:33:25 +0100

    lol debug

--- src/vendor/vendor.module.ts
+++ src/vendor/vendor.module.ts
@@ -4,6 +4,7 @@ import { VendingController } from './vendor.controller';
 import { ConfigModule } from '@nestjs/config';
 import { VendingService } from './vendor.service';
 import { NotificationModule } from 'src/notification/notification.module';
+import { MailModule } from 'src/common/services/mail.module';
 import { NotificationSettingsModule } from 'src/notification-settings/notification-settings.module';
 import { WalletModule } from 'src/wallet/wallet.module';
 import { PushNotificationModule } from 'src/push-notification/push-notification.module';
@@ -16,7 +17,8 @@ import { PushNotificationModule } from 'src/push-notification/push-notification.
     PushNotificationModule,
     ConfigModule,
     NotificationModule,
-    NotificationSettingsModule
+    NotificationSettingsModule,
+    MailModule,
   ],
   controllers: [VendingController],
   providers: [VendingService],
